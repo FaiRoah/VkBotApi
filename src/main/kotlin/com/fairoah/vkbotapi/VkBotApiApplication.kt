782f746e735c505a7598c55d0ac702b4ed09dc3a
Faizullin Roman <job.roman.faizullin@gmail.com>
2024-05-28 02:33:43 +0300

    [NF] init commit

---
+++ src/main/kotlin/com/fairoah/vkbotapi/VkBotApiApplication.kt
@@ -0,0 +1,11 @@
+package com.fairoah.vkbotapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class VkBotApiApplication
+
+fun main(args: Array<String>) {
+    runApplication<VkBotApiApplication>(*args)
+}
---
+++ src/test/kotlin/com/fairoah/vkbotapi/VkBotApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.fairoah.vkbotapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class VkBotApiApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
