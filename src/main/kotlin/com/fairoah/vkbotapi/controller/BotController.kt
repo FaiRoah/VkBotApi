d91b61e4be7c1591b23e061ad49ac58d454b8052
Faizullin Roman <job.roman.faizullin@gmail.com>
2024-05-28 21:48:41 +0300

    [NF] Add logging feature

--- src/main/kotlin/com/fairoah/vkbotapi/controller/BotController.kt
+++ src/main/kotlin/com/fairoah/vkbotapi/controller/BotController.kt
@@ -4,6 +4,7 @@ import com.fairoah.vkbotapi.model.EventDto
 import com.fairoah.vkbotapi.service.BotService
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import com.fasterxml.jackson.module.kotlin.readValue
+import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.http.HttpStatus
@@ -18,6 +19,7 @@ import org.springframework.web.bind.annotation.RestController
 @RequestMapping("/api")
 class BotController {
 
+    private val logger = LoggerFactory.getLogger(BotController::class.java)
 
     private lateinit var botService: BotService
 
@@ -33,15 +35,27 @@ class BotController {
     fun verifyServer(@RequestBody body: String): ResponseEntity<String> {
         val objectMapper = jacksonObjectMapper()
 
+        val eventDto: EventDto
+        try {
+            eventDto = objectMapper.readValue(body)
+        } catch (e: Exception) {
+            logger.error("Error parsing request body: $body", e)
+            return ResponseEntity.status(HttpStatus.OK).body("ok")
+        }
+
         return try {
-            val eventDto: EventDto = objectMapper.readValue(body)
             if (eventDto.type == "message_new") {
-                println(eventDto)   // TODO добавить логирование
-                botService.repeatText(eventDto.`object`?.message!!)
+                val message = eventDto.`object`?.message
+                if (message != null) {
+                    logger.info("New message: $message")
+                    botService.repeatText(message)
+                } else {
+                    logger.error("Got new message but it's null. ")
+                }
             }
             ResponseEntity.status(HttpStatus.OK).body("ok")
-        } catch (e: Exception) {
-            e.printStackTrace()
+        } catch (e: RuntimeException) {
+            logger.error("Error repeating message: ${eventDto.`object`?.message}", e)
             ResponseEntity.status(HttpStatus.OK).body("ok")
         }
 
--- src/main/kotlin/com/fairoah/vkbotapi/service/BotService.kt
+++ src/main/kotlin/com/fairoah/vkbotapi/service/BotService.kt
@@ -1,7 +1,9 @@
 package com.fairoah.vkbotapi.service
 
+import com.fairoah.vkbotapi.controller.BotController
 import com.fairoah.vkbotapi.model.MessageDto
 import com.squareup.okhttp.*
+import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
 import java.io.IOException
@@ -11,7 +13,7 @@ import kotlin.random.Random
 @Service
 class BotService (@Value("\${vk.accessToken}") val vkAccessToken: String){
 
-    val baseUrl: String = "https://api.vk.com/method/"
+    private val logger = LoggerFactory.getLogger(BotController::class.java)
 
     val apiVersion: Double = 5.236
 
@@ -41,12 +43,15 @@ class BotService (@Value("\${vk.accessToken}") val vkAccessToken: String){
 
         client.newCall(request).enqueue(object : Callback {
             override fun onFailure(request: Request?, e: IOException) {
+                logger.error("Request failed: $request.")
                 e.printStackTrace()
             }
 
             override fun onResponse(response: Response) {
-                if (!response.isSuccessful) throw IOException("Unexpected code $response")
-
+                if (!response.isSuccessful){
+                    logger.error("Response is unsuccessful: $response")
+                    throw IOException("Unexpected code $response")
+                }
             }
 
         })
